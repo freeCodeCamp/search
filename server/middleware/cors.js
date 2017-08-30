ace7b15276ec2170045d00b435ee74031fc3e467
Stuart Taylor <staylor1012@gmail.com>
2017-08-30 17:14:45 +0100

    Change out helmet-csp for cors (#27)

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,48 +1,18 @@
-const csp = require('helmet-csp');
+const cors = require('cors');
 
-const allowedOrigins = [
-  "'self'",
-  'beta.freecodecamp.com',
-  'beta.freecodecamp.org',
-  'guide.freecodecamp.com',
-  'guide.freecodecamp.org',
-  'freecodecamp.org',
-  'freecodecamp.com',
-  'guide.netlify.com',
-  'localhost:8000'
+const whitelist = [
+  'http://guide.freecodecamp.org',
+  'https://guide.freecodecamp.org',
+  'http://localhost:8000'
 ];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
-module.exports = csp({
-  // Specify directives as normal.
-  directives: {
-    defaultSrc: allowedOrigins,
-    scriptSrc: ["'none'"],
-    styleSrc: ["'none'"],
-    fontSrc: ["'none'"],
-    imgSrc: ["'none'"],
-    sandbox: true,
-    objectSrc: ["'none'"],
-    // upgradeInsecureRequests: true ** Wait for https **
-  },
-
-  // This module will detect common mistakes in your directives and throw errors
-  // if it finds any. To disable this, enable "loose mode".
-  loose: false,
-
-  // Set to true if you only want browsers to report errors, not block them.
-  // You may also set this to a function(req, res) in order to decide dynamically
-  // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
-  reportOnly: false,
-
-  // Set to true if you want to blindly set all headers: Content-Security-Policy,
-  // X-WebKit-CSP, and X-Content-Security-Policy.
-  setAllHeaders: false,
-
-  // Set to true if you want to disable CSP on Android where it can be buggy.
-  disableAndroid: false,
-
-  // Set to false if you want to completely disable any user-agent sniffing.
-  // This may make the headers less compatible but it will be much faster.
-  // This defaults to `true`.
-  browserSniff: true
-});
+module.exports = cors(corsOptions);
