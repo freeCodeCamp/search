b09aef50bd2bf1eeb362a42cfb066292a1edaf11
Stuart Taylor <staylor1012@gmail.com>
2017-08-30 07:08:50 +0100

    Use helmet-csp

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,20 +1,48 @@
-module.exports = function allowCrossDomain(req, res, next) {
-  const allowedOrigins = [
-    'beta.freecodecamp.com',
-    'beta.freecodecamp.org',
-    'freecodecamp.org',
-    'freecodecamp.com',
-    'netlify.com',
-    'guide.netlify.com'
-  ];
-  if (allowedOrigins.includes(req.hostname)) {
-    res.header('Access-Control-Allow-Origin', `${req.protocol}://${req.hostname}/*`);
-  } else {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  }
-  res.header('Access-Control-Allow-Methods', 'GET');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', 'true');
+const csp = require('helmet-csp');
 
-  next();
-};
+const allowedOrigins = [
+  "'self'",
+  'beta.freecodecamp.com',
+  'beta.freecodecamp.org',
+  'guide.freecodecamp.com',
+  'guide.freecodecamp.org',
+  'freecodecamp.org',
+  'freecodecamp.com',
+  'guide.netlify.com',
+  'localhost:8000'
+];
+
+module.exports = csp({
+  // Specify directives as normal.
+  directives: {
+    defaultSrc: allowedOrigins,
+    scriptSrc: [],
+    styleSrc: [],
+    fontSrc: [],
+    imgSrc: [],
+    sandbox: [],
+    objectSrc: [],
+    // upgradeInsecureRequests: true ** Wait for https **
+  },
+
+  // This module will detect common mistakes in your directives and throw errors
+  // if it finds any. To disable this, enable "loose mode".
+  loose: false,
+
+  // Set to true if you only want browsers to report errors, not block them.
+  // You may also set this to a function(req, res) in order to decide dynamically
+  // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
+  reportOnly: false,
+
+  // Set to true if you want to blindly set all headers: Content-Security-Policy,
+  // X-WebKit-CSP, and X-Content-Security-Policy.
+  setAllHeaders: false,
+
+  // Set to true if you want to disable CSP on Android where it can be buggy.
+  disableAndroid: false,
+
+  // Set to false if you want to completely disable any user-agent sniffing.
+  // This may make the headers less compatible but it will be much faster.
+  // This defaults to `true`.
+  browserSniff: true
+});
