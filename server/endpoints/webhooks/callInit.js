479b86e05bc363eb5cf617ab1087028a8fb65db2
Stuart Taylor <staylor1012@gmail.com>
2017-11-16 16:25:57 +0000

    Use path.resolve for callInit (#53)

--- server/endpoints/webhooks/callInit.js
+++ server/endpoints/webhooks/callInit.js
@@ -1,15 +1,16 @@
+const path = require('path');
 const spawn = require('child_process').spawn;
 
+const initPath = path.resolve(__dirname, '../../../init');
+
 function callInit() {
-  const init = spawn('node',[`${__dirname + '/../../../'}init`]);
+  const init = spawn('node',[ initPath ]);
   init.stdout.on('data', function (data) {
     console.log('stdout: ' + data.toString());
   });
-
   init.stderr.on('data', function (data) {
     console.log('stderr: ' + data.toString());
   });
-
   init.on('exit', function (code) {
     console.log('spawned child process "node init" exited with code ' + code.toString());
   });
