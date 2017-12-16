3c7246f0de4da91c1f19d3562ebcf240347c2456
Stuart Taylor <staylor1012@gmail.com>
2017-12-16 11:57:26 +0000

    Update (#59)
    * General refactor
    
    * Prep to move search behind a v1 endpoint
    
    * Comment out news router until News Rollout
    
    * Comment out news data source until the News Rollout
    
    * CORS all the routes
    
    * expand cors
    
    * Make build script

--- server/index.js
+++ server/index.js
@@ -8,11 +8,15 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const Rx = require('rx');
 const pmx = require('pmx');
-
+const { findTheThings } = require('../elastic');
+const { log } = require('../utils');
+const logger = log('server');
+const PORT = process.env.PORT || 7000;
+const { Observable } = Rx;
 const searchRouter = require('./endpoints/search');
 const webhookRouter = require('./endpoints/webhooks');
 const newsRouter = require('./endpoints/news');
-
+const { cors, options } = require('./middleware/cors');
 const app = express();
 const probe = pmx.probe();
 
@@ -31,15 +35,8 @@ const reqPerSec = probe.meter({
 
 const probes = [ reqPerHour, reqPerMin, reqPerSec ];
 
-const { findTheThings } = require('../elastic');
-const cors = require('./middleware/cors');
-const { log } = require('../utils');
 
-const logger = log('server');
-
-const PORT = process.env.PORT || 7000;
-
-const { Observable } = Rx;
+app.all('*', cors(options));
 
 app.use('*', (req, res, next) => {
   probes.forEach(probe => probe.mark());
@@ -60,7 +57,7 @@ app.use(bodyParser.json());
 app.set('views', __dirname +'/views');
 app.set('view engine', 'pug');
 
-// this route is to be removed when we update guides to use the v1 route
+// this route is to be removed when we update guides and FCCSearchBar to use the v1 route
 app.get('/search', cors, (req, res) => {
   const { q: query } = req.query;
   Observable.fromPromise(findTheThings(query))
