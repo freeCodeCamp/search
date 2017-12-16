b3e78e9b07eb8ba0f160a187eb5229c8d50b77b1
Stuart Taylor <staylor1012@gmail.com>
2017-12-16 15:05:51 +0000

    Fix CORS and move /views

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,13 +1,27 @@
 const cors = require('cors');
 
-const whitelist = [
-  /(\.|https:\/\/)freecodecamp\.org/, // freecodecamp.org and any subdomain of
-  /\.netlify\.com/, // deploy previews
+const whitelistStrings = [
   'http://localhost:3000', // fcc dev
   'http://localhost:8000' // guides dev
 ];
+
+const whitelistREs = [
+  /(\.|https:\/\/)freecodecamp\.org/, // freecodecamp.org and any subdomain of
+  /\.netlify\.com/, // deploy previews
+];
+
+function isTrustedOrigin(origin, callback) {
+  if (
+    whitelistREs.some(re => re.test(origin)) ||
+    whitelistStrings.indexOf(origin) !== -1
+  ) {
+    callback(null, true);
+  } else {
+    callback(new Error('The request is not from an authorised origin'));
+  }
+}
 const corsOptions = {
-  origin: whitelist,
+  origin: isTrustedOrigin,
   optionsSuccessStatus: 200
 };
 exports.cors = cors;
